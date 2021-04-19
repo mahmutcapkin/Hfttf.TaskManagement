51177f91fdd39a200a93534d00f0352a8fcf124c
Mahmut Çapkın <mahmut.capkin@izersoft.com>
2021-04-19 17:27:16 +0300

    updating views

--- Hfttf.TaskManagement.UI/Controllers/ProfileInfoController.cs
+++ Hfttf.TaskManagement.UI/Controllers/ProfileInfoController.cs
@@ -16,12 +16,155 @@ namespace Hfttf.TaskManagement.UI.Controllers
             _addressService = addressService;
         }
 
-        [JwtAuthorize(Roles = "Admin,User")]
+        //[JwtAuthorize(Roles = "Admin,User")]
         public async Task<IActionResult> Index()
         {
-            var data = await _addressService.GetAllAsync();
-            return View(data);
+            //var data = await _addressService.GetAllAsync();
+            return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> EditProfile()
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditProfile(int id)
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteProfile()
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteProfile(int id)
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> EditBankInfo()
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditBankInfo(int id)
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteBankInfo()
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteBankInfo(int id)
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditEducationInfo()
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditEducationInfo(int id)
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteEducationInfo()
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteEducationInfo(int id)
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditEmergencyInfo()
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditEmergencyInfo(int id)
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteEmergencyInfo()
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteEmergencyInfo(int id)
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> EditExperience()
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditExperience(int id)
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteExperience()
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteExperience(int id)
+        {
+            //var data = await _addressService.GetAllAsync();
+            return View();
+        }
+
 
         public async Task<IActionResult> GetById(int id)
         {
