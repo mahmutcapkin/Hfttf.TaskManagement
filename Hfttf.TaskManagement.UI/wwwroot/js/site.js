8648c1c293dadedb9c5151a6cbd156db9b06b2b1
Mahmut Çapkın <mahmut.capkin@izersoft.com>
2021-05-02 19:08:36 +0300

    no message

--- Hfttf.TaskManagement.UI/wwwroot/js/site.js
+++ Hfttf.TaskManagement.UI/wwwroot/js/site.js
@@ -34,9 +34,9 @@ showInPopup = (url, title) => {
         type: 'GET',
         url: url,
         success: function (res) {
-            $('#form-modal .modal-body').html(res);
-            $('#form-modal .modal-title').html(title);
-            $('#form-modal').modal('show');
+            $('#address-modal .modal-body').html(res);
+            $('#address-modal .modal-title').html(title);
+            $('#address-modal').modal('show');
             // to make popup draggable
             $('.modal-dialog').draggable({
                 handle: ".modal-header"
@@ -57,12 +57,12 @@ jQueryAjaxPost = form => {
             success: function (res) {
                 if (res.isValid) {
                     $('#address').html(res.html)
-                    $('#form-modal .modal-body').html('');
-                    $('#form-modal .modal-title').html('');
-                    $('#form-modal').modal('hide');
+                    $('#address-modal .modal-body').html('');
+                    $('#address-modal .modal-title').html('');
+                    $('#address-modal').modal('hide');
                 }
                 else
-                    $('#form-modal .modal-body').html(res.html);
+                    $('#address-modal .modal-body').html(res.html);
             },
             error: function (err) {
                 console.log(err)
@@ -87,7 +87,7 @@ jQueryAjaxDelete = form => {
                 contentType: false,
                 processData: false,
                 success: function (res) {
-                    $('#view-all').html(res.html);
+                    $('#address').html(res.html);
                 },
                 error: function (err) {
                     console.log(err)
